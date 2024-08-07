9caac28ba6d4a6eca9c512b92adfa47c3a6fa2b6
Kelechi <ebirikele@gmail.com>
2024-08-07 22:38:56 +0100

    Resloving multiple processes

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,5 +5,5 @@ export default function handleProfileSignup() {
   Promise.all(promises)
     .then((results) => {
       console.log(results[0].body, results[1].firstName, results[1].lastName);
-    }).catch((error) => console.error(error));
+    }).catch(() => console.error('Signup system offline'));
 }
