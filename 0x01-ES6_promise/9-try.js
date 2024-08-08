b26b67957509b9ed0338b70a0f3ea0d2238da2d3
Kelechi <ebirikele@gmail.com>
2024-08-08 02:23:31 +0100

    try catch error

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,11 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const returnedValue = mathFunction();
+    queue.push(returnedValue);
+  } catch (error) {
+    queue.push(error);
+  }
+  queue.push('Guardrail was processed');
+  return queue;
+}
