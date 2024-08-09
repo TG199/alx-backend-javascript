6a8064e8135c701d4987ab736729c85c989ad469
Kelechi <ebirikele@gmail.com>
2024-08-09 03:32:18 +0100

    string representation

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,18 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new Error('TypeError: name must be a string');
+    }
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new Error('TypeError: name must be a string');
+    }
+  }
+
+  toString() {
+    return `${this._code}`;
+  }
+}
