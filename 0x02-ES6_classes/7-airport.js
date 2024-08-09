21cff66ff46b80f3350d869759f13de5bcbdf9c0
Kelechi <ebirikele@gmail.com>
2024-08-09 03:39:58 +0100

    class rep

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -3,16 +3,40 @@ export default class Airport {
     if (typeof name === 'string') {
       this._name = name;
     } else {
-      throw new Error('TypeError: name must be a string');
+      throw new Error('TypeError: Name must be a string');
     }
     if (typeof code === 'string') {
       this._code = code;
     } else {
-      throw new Error('TypeError: name must be a string');
+      throw new Error('TypeError: Code must be a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new Error('TypeError: Name must be a string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value === 'string') {
+      this._code = value;
+    } else {
+      throw new Error('TypeError: Code must be a string');
     }
   }
 
   toString() {
-    return `${this._code}`;
+    return this._code;
   }
 }
