89db870da463ba6c5e1355d1e9f5108eab8fd651
Kelechi <ebirikele@gmail.com>
2024-08-13 04:45:38 +0100

    Return list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,18 +1,19 @@
 export default function getListStudents() {
-  const guillame = {
-    id: 1,
-    firstName: 'Guillame',
-    location: 'San Francisco',
-  };
-  const james = {
-    id: 2,
-    firstName: 'James',
-    location: 'Columbia',
-  };
-  const serena = {
-    id: 5,
-    firstName: 'Serena',
-    location: 'San Francisco',
-  };
-  return [guillame, james, serena];
+  return [
+    {
+      id: 1,
+      firstName: 'Guillame',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
 }
