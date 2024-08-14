49c91ea6bce02f1cb90b16dfebc90699acda1659
Kelechi <ebirikele@gmail.com>
2024-08-14 02:47:02 +0100

    error handling

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,11 @@
 export default function updateUniqueItems(map) {
+    if (typeof map != Map)
+    {
+        throw new Error("Cannot process")
+    }
   for (const [key, value] of map.entries()) {
     if (value === 1) {
       map.set(key, 100);
-    } else {
-      throw new Error('Cannot process');
     }
   }
   return map;
